e0110dd8c7c0a0fa811eccf6ca749704a9a35904
navas <gabrielnavas@email.com>
2024-07-16 16:16:47 -0300

    example card inside app component

--- section-3/card-example-project/src/app/app.module.ts
+++ section-3/card-example-project/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CardComponent} from "./card/card.component";
 
 @NgModule({
   declarations: [
@@ -17,4 +17,5 @@ import { CardComponent } from './card/card.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
