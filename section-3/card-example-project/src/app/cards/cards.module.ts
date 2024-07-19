a441a069082955eecc9e3dde5ba95ebd30b438df
navas <gabrielnavas@email.com>
2024-07-19 17:06:07 -0300

    change all component with ng-deep

--- section-3/card-example-project/src/app/cards/cards.module.ts
+++ section-3/card-example-project/src/app/cards/cards.module.ts
@@ -4,7 +4,7 @@ import {CardComponent} from "../card/card.component";
 import {CardPurpleComponent} from "../card-purple/card-purple.component";
 import {CardButtonComponent} from "../card-button/card-button.component";
 import {CardPurpleButtonComponent} from "../card-purple-button/card-purple-button.component";
-import { CardButtonCancelComponent } from './card-button-cancel/card-button-cancel.component';
+import {CardButtonCancelComponent} from './card-button-cancel/card-button-cancel.component';
 
 
 @NgModule({
@@ -21,6 +21,7 @@ import { CardButtonCancelComponent } from './card-button-cancel/card-button-canc
   exports: [
     CardComponent,
     CardPurpleComponent,
+    CardButtonCancelComponent,
   ]
 })
 export class CardsModule {
