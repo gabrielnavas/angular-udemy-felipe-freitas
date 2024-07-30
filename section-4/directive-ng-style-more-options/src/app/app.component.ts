497bdbd655ddc764eb9d05d4842912da76b1e367
navas <gabrielnavas@email.com>
2024-07-30 15:51:59 -0300

    style object

--- section-4/directive-ng-style-more-options/src/app/app.component.ts
+++ section-4/directive-ng-style-more-options/src/app/app.component.ts
@@ -9,16 +9,23 @@ export class AppComponent {
 
   fontSize: number = 15;
   textColor: 'red' | 'blue' = 'red';
+  
   stylesString: string = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor,
+  }
 
   increaseFontSize() {
     this.fontSize += 4;
     this.updateStyleString();
+    this.updateStyleObj();
   }
 
   decreaseFontSize() {
     this.fontSize -= 4;
     this.updateStyleString();
+    this.updateStyleObj();
   }
 
   toggleTextFontColor() {
@@ -28,10 +35,18 @@ export class AppComponent {
       this.textColor = 'red';
     }
     this.updateStyleString();
+    this.updateStyleObj();
+  }
+
+  private updateStyleObj() {
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor,
+    }
   }
 
   private updateStyleString() {
-    // its force update the H3
+    // its force update the H4
     this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
   }
 }
