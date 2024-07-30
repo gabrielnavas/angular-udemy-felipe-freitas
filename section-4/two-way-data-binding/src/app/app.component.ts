b04dc2590435f4b6f28c40c2954f94062b4b1805
navas <gabrielnavas@email.com>
2024-07-30 01:23:09 -0300

    ng model two way data binding

---
+++ section-4/two-way-data-binding/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  name = '';
+
+  handleInputChange(value: String) {
+    console.log(value);
+  }
+}
