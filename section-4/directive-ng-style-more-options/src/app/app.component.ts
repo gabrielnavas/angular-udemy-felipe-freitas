ea5ee225087c7938dcc653b6048ae70ec0c88172
navas <gabrielnavas@email.com>
2024-07-30 15:41:23 -0300

    toggle text on ng style and show more options

--- section-4/directive-ng-style-more-options/src/app/app.component.ts
+++ section-4/directive-ng-style-more-options/src/app/app.component.ts
@@ -9,13 +9,16 @@ export class AppComponent {
 
   fontSize: number = 15;
   textColor: 'red' | 'blue' = 'red';
+  stylesString: string = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
 
   increaseFontSize() {
     this.fontSize += 4;
+    this.updateStyleString();
   }
 
   decreaseFontSize() {
     this.fontSize -= 4;
+    this.updateStyleString();
   }
 
   toggleTextFontColor() {
@@ -24,5 +27,11 @@ export class AppComponent {
     } else {
       this.textColor = 'red';
     }
+    this.updateStyleString();
+  }
+
+  private updateStyleString() {
+    // its force update the H3
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
   }
 }
