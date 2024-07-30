1d886127f7384d6dc508ab5e48d8544a58fe8062
navas <gabrielnavas@email.com>
2024-07-29 23:28:15 -0300

    add card component

--- section-4/project-card/src/app/components/card/card.component.ts
+++ section-4/project-card/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  @Input({ required: true }) planType = '';
+  @Input({ required: true }) price = 0.00;
+  @Input({ alias: 'style' }) cardStyle: 'orange' | 'purple' = 'orange';
 
+  @Input({ required: true }) buttonText = "";
+  @Input({ required: true }) buttonStyle: 'white' | 'purple' = 'white'; 
+  @Output('cliked') buttonClikedEmitter = new EventEmitter();
+
+  onCliked() {
+    this.buttonClikedEmitter.emit();
+  }
 }
