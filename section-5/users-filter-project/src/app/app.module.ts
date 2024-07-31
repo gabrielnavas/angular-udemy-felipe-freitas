5ceff5e6ffd7a0a0884bab3a7050ba2b80f72d20
navas <gabrielnavas@email.com>
2024-07-30 22:10:31 -0300

    add angular material ui

--- section-5/users-filter-project/src/app/app.module.ts
+++ section-5/users-filter-project/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +14,11 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- section-5/users-filter-project/src/app/components/components.module.ts
+++ section-5/users-filter-project/src/app/components/components.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
