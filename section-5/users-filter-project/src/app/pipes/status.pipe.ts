0524356dce7edbef5f00c2468c555934a54cbace
navas <gabrielnavas@email.com>
2024-08-01 11:05:08 -0300

    refactory status pipe

--- section-5/users-filter-project/src/app/pipes/status.pipe.ts
+++ section-5/users-filter-project/src/app/pipes/status.pipe.ts
@@ -5,9 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StatusPipe implements PipeTransform {
   transform(status: boolean): string {
+    if(!this.isValid(status)) {
+      return this.generateInvalidMessage();
+    }
+    return this.currentStatus(status);
+  }
+
+  private generateInvalidMessage() {
+    return 'Status inválido ou indisponível';
+  }
+
+  private isValid(status: boolean) {
+    if(status === undefined || status === null) {
+      return false
+    }
+
+    return true;
+  }
+
+  private currentStatus(status: boolean) {
     return status 
       ? "Ativo" 
       : "Inativo";
   }
-
 }
