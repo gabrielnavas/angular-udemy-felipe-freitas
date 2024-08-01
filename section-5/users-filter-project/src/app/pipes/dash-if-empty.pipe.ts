e90529409ca9291d2428ab9475841ca78aa4c183
navas <gabrielnavas@email.com>
2024-08-01 11:39:00 -0300

    dash if empty on date

--- section-5/users-filter-project/src/app/pipes/dash-if-empty.pipe.ts
+++ section-5/users-filter-project/src/app/pipes/dash-if-empty.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DashIfEmptyPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: any): string {
     if(value === "" || value === undefined || value === null) {
       return "-";
     }
