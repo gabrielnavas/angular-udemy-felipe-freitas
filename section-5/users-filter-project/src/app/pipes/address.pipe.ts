edb09e4f40e0a9c526534bd0371ac5e1c0ca7a59
navas <gabrielnavas@email.com>
2024-08-01 10:57:58 -0300

    refactory address pipe

--- section-5/users-filter-project/src/app/pipes/address.pipe.ts
+++ section-5/users-filter-project/src/app/pipes/address.pipe.ts
@@ -6,10 +6,39 @@ import { IAddress } from '../interfaces/user/address.interface';
 })
 export class AddressPipe implements PipeTransform {
   transform(address: IAddress): string {
+    if(!this.isValid(address)) {
+      return this.generateAddressInvalidMessage();
+    }
     const street = address.rua;
     const number = address.numero;
     const city = address.cidade;
     const state = address.estado;
     return `${street}, ${number}, ${city}, ${state}`;
+  }
+
+  private generateAddressInvalidMessage() {
+    return 'Endereço indisponível ou inválido';
+  }
+
+  private isValid(address: IAddress): boolean {
+    if(!address) {
+      return false;
+    }
+    if(!address.rua) {
+      return false;
+    }
+    if(!address.numero) {
+      return false;
+    }
+    if(!address.cidade) {
+      return false;
+    }
+    if(!address.estado) {
+      return false;
+    }
+    if(!address.pais) {
+      return false;
+    }
+    return true;
   }
 }
